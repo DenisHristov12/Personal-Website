3527a7c075b9824d1960b69258c1969ad7916525
DenisHristov <48285397+DenisHristov12@users.noreply.github.com>
2024-02-07 21:12:46 +0200

    experimenting with some styles

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,15 +1,38 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledAppLayoyt = styled.div``;
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StyledRight = styled.div`
+  display: flex;
+  gap: 5px;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  /* padding: 0px 10px;
+  border-right: solid black 1px; */
+`;
 
 function AppLayout() {
   return (
     <div>
       {/* style header and the links */}
-      <header>
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/about">About me</NavLink>
-        <NavLink to="/skills">Skills</NavLink>
-        <NavLink to="/contacts">Contact me</NavLink>
-      </header>
+      <StyledHeader>
+        <div>
+          <StyledNavLink to="/home">Denis Hristov</StyledNavLink>
+        </div>
+        <StyledRight>
+          <StyledNavLink to="/about">About me</StyledNavLink>
+          <StyledNavLink to="/skills">Skills</StyledNavLink>
+          <StyledNavLink to="/contacts">Contact me</StyledNavLink>
+        </StyledRight>
+      </StyledHeader>
 
       <main>
         <Outlet />
