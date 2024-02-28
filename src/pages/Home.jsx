99a5850c6b25bce4b7ced538a123ddd8fee82816
DenisHristov12 <48285397+DenisHristov12@users.noreply.github.com>
2024-02-28 19:43:14 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,58 +1,59 @@
 import styled from 'styled-components';
 
 // style home page
-const StyledHome = styled.div`
-  height: 500px;
-  margin-top: 110px;
-  /* height: 100vh; */
-  display: flex;
-  /* justify-content: space-between; */
-  gap: 300px;
-  align-items: center;
-  flex-direction: row;
-  /* overflow-y: scroll; */
-  background-color: var(--color-yellow-400);
-  border-radius: 10px;
-`;
+// const StyledHome = styled.div`
+//   height: 500px;
+//   margin-top: 110px;
+//   /* height: 100vh; */
+//   display: flex;
+//   /* justify-content: space-between; */
+//   gap: 300px;
+//   align-items: center;
+//   flex-direction: row;
+//   /* overflow-y: scroll; */
+//   background-color: var(--color-yellow-400);
+//   border-radius: 10px;
+// `;
 
-const Div = styled.div`
-  text-align: center;
-  background-color: var(--color-yellow-200);
-  color: var(--color-grey-700);
-  /* background-color: red; */
-  /* flex: 1; */
-  width: 400px;
-  /* margin-top: 15%; */
-  margin-left: 10%;
-  border-radius: 32px;
-  padding: 10px 20px 20px;
-`;
+// const Div = styled.div`
+//   text-align: center;
+//   background-color: var(--color-yellow-200);
+//   color: var(--color-grey-700);
+//   /* background-color: red; */
+//   /* flex: 1; */
+//   width: 400px;
+//   /* margin-top: 15%; */
+//   margin-left: 10%;
+//   border-radius: 32px;
+//   padding: 10px 20px 20px;
+// `;
 
-const P = styled.p`
-  margin-top: 10px;
-`;
+// const P = styled.p`
+//   margin-top: 10px;
+// `;
 
-const ImgDiv = styled.div`
-  /* padding-top: 20px; */
-  /* margin-top: 8%; */
-  width: auto;
-  height: 350px;
-  overflow: hidden;
-  border-radius: 50%;
-  margin-right: 100px;
-`;
+// const ImgDiv = styled.div`
+//   /* padding-top: 20px; */
+//   /* margin-top: 8%; */
+//   width: auto;
+//   height: 350px;
+//   overflow: hidden;
+//   border-radius: 50%;
+//   margin-right: 100px;
+// `;
 
-const Img = styled.img`
-  /* margin-top: 10%; */
-  width: auto;
-  height: 650px;
-  margin-bottom: 50px;
-`;
+// const Img = styled.img`
+//   /* margin-top: 10%; */
+//   width: auto;
+//   height: 650px;
+//   margin-bottom: 50px;
+// `;
 
 function Home() {
   return (
-    <StyledHome>
-      <Div>
+    // <StyledHome>
+    {
+      /* <Div>
         My name is Denis.
         <P>
           I am a student at Technical University of Sofia who wants to improve
@@ -75,8 +76,9 @@ function Home() {
           src="DSC_1625.jpg"
           alt="Photo of Denis Hristov"
         />
-      </ImgDiv>
-    </StyledHome>
+      </ImgDiv> */
+    }
+    // </StyledHome>
   );
 }
 
